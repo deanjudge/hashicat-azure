9f033f0588b507b4fc738e4725f6dcd70714f8ca
deanjudge <97558478+deanjudge@users.noreply.github.com>
2022-03-25 14:32:10 +1100

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,7 +9,7 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><img src="https://img.ifunny.co/images/bd32e38960bb89b314b5664e6449d02785baa9d8047fa9a59557b124a57d9192_1.webp"></img></center>
   <center><h2>Meow World!</h2></center>
   Welcome to ${PREFIX}'s app. Meowdy Partner. Gee Haw 
   <!-- END -->
