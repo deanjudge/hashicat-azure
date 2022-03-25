cde23aa55caed10baf5b28f6734b7dca5c06b6d8
deanjudge <97558478+deanjudge@users.noreply.github.com>
2022-03-25 13:58:13 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -105,13 +105,15 @@ resource "azurerm_public_ip" "catapp-pip" {
 resource "azurerm_virtual_machine" "catapp" {
   name                = "${var.prefix}-meow"
   location            = var.location
-  Department          =  "devops"
+  
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   vm_size             = var.vm_size
 
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
-
+  tags = {
+    Department          =  "devops"
+  }
   storage_image_reference {
     publisher = var.image_publisher
     offer     = var.image_offer
